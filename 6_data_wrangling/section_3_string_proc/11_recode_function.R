97bcde6cdc35ba98a7a3a405cd2ded1ecd958e32
Marc H <marc.omar.haddad@gmail.com>
2019-12-21 18:56:22 -0500

    Completed 11_recode_function.R

---
+++ 6_data_wrangling/section_3_string_proc/11_recode_function.R
@@ -0,0 +1,37 @@
+
+library(dslabs)
+library(tidyverse)
+
+data("gapminder")
+
+# Plot Example
+gapminder %>%
+  filter(region=="Caribbean") %>%
+  ggplot(aes(year, life_expectancy, color = country)) +
+  geom_line()
+# The above plot shows an inefficient use of space to accomodate long country names
+
+# Here are the longest ones:
+gapminder %>%
+  filter(region=="Caribbean") %>%
+  filter(str_length(country) >= 12) %>%
+  distinct(country)
+# Antigua and Barbuda
+# Dominican Republic
+# St. Vincent and the Grenadines
+# Trinidad and Tobago
+
+# b/c the above names appear multiple times in the dataset
+# we need to pick nicknames and apply them to all instances of these countries
+
+# recode()
+gapminder %>% filter(region=="Caribbean") %>%
+  mutate(country = recode(country,
+                          `Antigua and Barbuda` = "Barbuda",
+                          `Dominican Republic` = "DR",
+                          `St. Vincent and the Grenadines` = "St. Vincent",
+                          `Trinidad and Tobago` = "Trinidad")) %>%
+  ggplot(aes(year, life_expectancy, color = country)) +
+  geom_line()
+# Note that we surround the long names with `` b/c we are nonstandard var names;
+# This way, we can change every instance of them.
