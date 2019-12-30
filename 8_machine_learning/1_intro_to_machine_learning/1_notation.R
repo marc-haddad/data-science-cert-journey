a41e7915e0b0447955270091d213ded1e87faf3a
Marc H <marc.omar.haddad@gmail.com>
2019-12-30 17:48:04 -0500

    Initial commit

---
+++ 8_machine_learning/1_intro_to_machine_learning/1_notation.R
@@ -0,0 +1,34 @@
+
+# In Machine Learning (ML) data comes in the form of
+# outcomes (Y) we wish to predict, & the features (X_1...X_p) we will use to predict the outcome
+# Features are also known as predictors, and covariates
+
+# Types of prediction problems: categorical, & continous
+
+# Categorical outcomes: Y can be any of K classes
+  # Ex: K = 10, w/ classes 0, 1, 2, 3, 4, 5, 6, 7, 8, and 9
+  # Ex: Speech recognission: outcomes are all possible words we're trying to detect
+
+# notation of K: k = 1, ..., K, and k = 0, 1 (if binary)
+
+# To predict an unknown outcome w/ features, we collect data for which we DO know the outcome
+
+# Y_hat denotes prediction
+# "Actual outcome" is used to denote what we ended up observing
+# We want Y_hat == actual outcome
+
+# ML task for categorical data is known as Classification,
+# where predictions will be categorical like our outcomes,
+# and they will be either right or wrong
+
+# ML task for continuous data is known as Prediction,
+# not measured as right or wrong,
+# instead measured w/ ERROR, the diff b/w our prediction and actual outcome
+
+
+# First step in writing ML algorithm:
+  # What are the outcomes?
+  # What are the features?
+
+
+
