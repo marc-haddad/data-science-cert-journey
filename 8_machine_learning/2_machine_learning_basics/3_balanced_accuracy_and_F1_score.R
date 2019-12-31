06d759e5d0a650687188405262c973f5285867c1
Marc H <marc.omar.haddad@gmail.com>
2019-12-31 17:35:57 -0500

    Completed 3_balanced_accuracy_and_F1_score.R

---
+++ 8_machine_learning/2_machine_learning_basics/3_balanced_accuracy_and_F1_score.R
@@ -0,0 +1,71 @@
+
+
+# Balanced Accuracy: the avg of specificity and sensitivity; A metric that is generally preferred to accuracy 
+
+# B/c speci. and sens. are rates, it is more appropriate to compute the harmonic avg of speci. and sens.:
+# F1_score = 1 / (1/2 * (1/recall + 1/precision)), also written as:
+# F1_score = 2 * precision * recall / (precision + recall)
+
+
+# Diff. situations require diff. considerations of what to pay particular importance to.
+# Ex: For airplane safety, it is more important to maximize sensitivity over specificity.
+#     Failing to predict a plane will malfunction before it crashes (FN) is a much more costly error than grounding a plane that is in perfect condition (FP)
+# Ex: In a capital murder case, it is more important to maximize specificity over sensitivity.
+#     Incorrectly predicting the guiltiness of an innocent person (FP) is much more costly than letting an actual murderer go (FN)
+
+# The F1 score can be altered to weigh sens. and speci. differently depending on the issue
+# We do this by defining Beta as how much more important sens. is than speci.
+# weighted_F1_score = 1 / ( (Beta^2/(1+Beta^2)*1/recall) + (1/(1+Beta^2)*1/precision) )
+
+# F_meas() in caret computes the above summary, w/ Beta defaulting to 1
+
+
+# Prediction algorithm rebuild, maximizing F score instead of overall accuracy:
+cutoff = seq(61, 70)
+F_1 = map_dbl(cutoff, function(x) {
+  y_hat = ifelse(train_set$height > x, "Male", "Female") %>% 
+    factor(levels = levels(test_set$sex))
+  F_meas(data = y_hat, reference = factor(train_set$sex))
+})
+cutoff
+df = data.frame(cutoff, F_1)
+df %>% ggplot(aes(cutoff, F_1)) + geom_point() + geom_line()
+max(F_1) # 0.614
+best_cutoff = cutoff[which.max(F_1)]
+best_cutoff # 66 (was 64 when maximizing for accuracy)
+
+y_hat = ifelse(test_set$height > best_cutoff, "Male", "Female") %>% 
+  factor(levels = levels(test_set$sex))
+confusionMatrix(data = y_hat, reference = test_set$sex)
+ 
+# Confusion Matrix and Statistics
+# 
+#         Reference
+# Prediction Female Male
+# Female     81   67
+# Male       38  339
+# 
+# Accuracy : 0.8           
+# 95% CI : (0.763, 0.833)
+# No Information Rate : 0.773         
+# P-Value [Acc > NIR] : 0.07819       
+# 
+# Kappa : 0.475         
+# 
+# Mcnemar's Test P-Value : 0.00629       
+#                                         
+#             Sensitivity : 0.681         
+#             Specificity : 0.835         
+#          Pos Pred Value : 0.547         
+#          Neg Pred Value : 0.899         
+#              Prevalence : 0.227         
+#          Detection Rate : 0.154         
+#    Detection Prevalence : 0.282         
+#       Balanced Accuracy : 0.758         
+#                                         
+#        'Positive' Class : Female     
+
+# By maximizing by F score our sens. and speci. are much more balanced than before
+
+# Conclusion: This is my first ML algorithm; 
+# It takes height as a predictor, and predicts that those w/ heights below 66 inches are going to be female.
